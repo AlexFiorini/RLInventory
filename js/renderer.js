f7391ddc5154db2d7fd398bdbd4f1dcf5b542fd3
AlexFiorini <61363883+AlexFiorini@users.noreply.github.com>
2023-08-11 22:41:15 +0200

    Commentato ricerca URL

--- js/renderer.js
+++ js/renderer.js
@@ -47,44 +47,41 @@ function createTable() {
             { title: "Rank Label", field: "rank_label", headerFilter: "input", sorter: "string", resizable: false},
             { title: "Quality", field: "quality", headerFilter: "input", sorter: "string", resizable: false},
             { title: "Special Edition", field: "special_edition", headerFilter: "input", sorter: "string", resizable: false},
-            { title: "RLInsider Price min", headerFilter:"input", sorter: "string", resizable: false},
-            { title: "RLInsider Price max", headerFilter:"input", sorter: "string", resizable: false}
+            // { title: "RLInsider Price min", headerFilter:"input", sorter: "string", resizable: false},
+            // { title: "RLInsider Price max", headerFilter:"input", sorter: "string", resizable: false}
         ],
     });
 
     //load sample data into the table
-    // ...
-
-    // ...
 
     table.on("tableBuilt", async function(){
         for (const item of inventory) {
             // Controlla se la proprietà "quality" non è vuota
             if (item.quality !== '' && item.quality !== 'Common') {
-                // Effettua lo swap se è necessario
-                const slotValue = (slotMapping[item.slot.toLowerCase()] || item.slot).replace(/-/g, '_');
-                const swappedSlotValue = slotValue === 'rocket_boost' ? 'boosts' : slotValue;
-                const itemName = item.name.replace(/-/g, '_');
+                // // Effettua lo swap se è necessario
+                // const slotValue = (slotMapping[item.slot.toLowerCase()] || item.slot).replace(/-/g, '_');
+                // const swappedSlotValue = slotValue === 'rocket_boost' ? 'boosts' : slotValue;
+                // const itemName = item.name.replace(/-/g, '_');
                 
-                // Forma l'URL completo per l'elemento
-                const itemUrl = `https://rl.insider.gg/en/pc/${swappedSlotValue}/${itemName}${item.paint !== 'none' ? `/${item.paint}` : ''}`;
+                // // Forma l'URL completo per l'elemento
+                // const itemUrl = `https://rl.insider.gg/en/pc/${swappedSlotValue}/${itemName}${item.paint !== 'none' ? `/${item.paint}` : ''}`;
                 
-                // Trasforma l'URL in minuscolo e rimuovi gli spazi
-                const lowercaseUrl = itemUrl.toLowerCase().replace(/\s+/g, '_');
+                // // Trasforma l'URL in minuscolo e rimuovi gli spazi
+                // const lowercaseUrl = itemUrl.toLowerCase().replace(/\s+/g, '_');
                 
-                // Esegui lo scraping dell'URL
-                const browser = await puppeteer.launch({ headless: 'new' });
-                const page = await browser.newPage();
-                await page.goto(lowercaseUrl, { waitUntil: 'domcontentloaded' });
+                // // Esegui lo scraping dell'URL
+                // const browser = await puppeteer.launch({ headless: 'new' });
+                // const page = await browser.newPage();
+                // await page.goto(lowercaseUrl, { waitUntil: 'domcontentloaded' });
     
-                const itemSummaryPrice = await page.evaluate(() => {
-                    const h1Element = document.querySelector('#itemSummaryPrice');
-                    return h1Element ? h1Element.textContent : null;
-                });
+                // const itemSummaryPrice = await page.evaluate(() => {
+                //     const h1Element = document.querySelector('#itemSummaryPrice');
+                //     return h1Element ? h1Element.textContent : null;
+                // });
     
-                console.log(`Valore di h1 id="itemSummaryPrice" per ${item.name} ${item.paint}:`, itemSummaryPrice);
+                // console.log(`Valore di h1 id="itemSummaryPrice" per ${item.name} ${item.paint}:`, itemSummaryPrice);
     
-                await browser.close();
+                // await browser.close();
                 
                 // Aggiungi la riga alla tabella
                 table.addRow(item);
